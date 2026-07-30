ff140094cf25ce7cd970b4cd24d969a07a644fd0
adocxwork <adipracc@gmail.com>
2026-07-30 19:08:22 +0530

    Configure Vite to output to root for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../',
+    emptyOutDir: false, // Prevents wiping the root directory!
+  }
 })
